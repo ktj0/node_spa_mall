48ffc7d7c68929b9e970fb6e9cc17b5f167b2296
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-14 13:29:51 +0900

    메인페이지 Hello World! 추가

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ connect();
 app.use(express.json());
 app.use("/api", [goodsRouter, cartsRouter]);
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
 });
